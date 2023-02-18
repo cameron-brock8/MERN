e39101bf0845127c72bbba8a8b299671e5fbb0d7
Cameron Brock <ckb19b@fsu.edu>
2023-02-18 17:19:09 -0500

    Adding more functionality

---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
+
+const ProductScreen = ({match} ) => {
+    const product = products.find(p => p.id === match.params.id)
+
+  return ( 
+    <Link className='btn btn-light my-3' to='/'> Go Back  
+    <Row>
+        <Col md={6}>
+            <Image src={product.image} alt={product.name} />
+        </Col>
+        <Col md={3}>
+            <ListGroup variant='flush'>
+                <ListGroup.Item>
+                    <h2>{product.name}</h2>
+                </ListGroup.Item>
+            </ListGroup>
+        </Col>
+    </Row>
+    </Link>
+  )
+}
+
+export default ProductScreen
